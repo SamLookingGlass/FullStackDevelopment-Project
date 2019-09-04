10f60100d882e4e1a1c40520a8d2b95ac1f9d069
Ubuntu <ubuntu@ip-172-31-41-89.ec2.internal>
2019-09-04 04:25:42 +0000

    Added one-to-many relationship between items and category models

--- crud/models.py
+++ crud/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=30, blank=False)
-    done = models.BooleanField(blank=False, default=True)
-
+    done = models.BooleanField(blank=False, default=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+        
+        
+class Category(models.Model):
+    name = models.CharField(max_length=255, blank=False)
     def __str__(self):
         return self.name
--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
 from crud.views import show_todos, create_item, edit_item
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('crud/', show_todos, name='show_todos'),
     path('crud/create', create_item),
     path('crud/edit/<id>', edit_item),
