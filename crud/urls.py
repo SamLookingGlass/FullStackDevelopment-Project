0d0d680f12578d051df4f91b9496626715961dcf
Ubuntu <ubuntu@ip-172-31-41-89.ec2.internal>
2019-09-04 09:56:36 +0000

    Added vendor static templates

--- crud/urls.py
+++ crud/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from crud.views import show_todos, create_item, edit_item
+from crud.views import show_todos, create_item, edit_item, item_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('crud/', show_todos, name='show_todos'),
     path('crud/create', create_item),
     path('crud/edit/<id>', edit_item),
+    path('crud/items', item_list, name="item-list")
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .forms import ItemForm
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
-
-
 from .models import Item
 
 # Create your views here.
@@ -34,3 +32,9 @@ def edit_item(request, id):
     else:
         form = ItemForm(instance=item)
     return render(request, "create_items.html", {'form': form})
+    
+def item_list(request):
+    context = {
+        'items' : Item.objects.all()
+    }
+    return render(request, "home-page.html", context)
