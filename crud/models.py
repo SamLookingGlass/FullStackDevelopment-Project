c795ec6323f1a7b23eeed723db327b1256dc74e9
Ubuntu <ubuntu@ip-172-31-41-89.ec2.internal>
2019-09-04 09:19:28 +0000

    Added price field to 'Item' model and added 2 new models 'OrderItems' and 'Order'

--- crud/models.py
+++ crud/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=30, blank=False)
+    price = models.FloatField(null=True)
     # Linked in admin.py
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     # Linked in admin.py
@@ -20,3 +22,18 @@ class Tag(models.Model):
     name = models.CharField(max_length=50, blank=False)
     def __str__(self):
         return self.name        
+
+# Link between 2 models
+class OrderItem(models.Model):
+    def __str__(self):
+        return self.name        
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name        
+
