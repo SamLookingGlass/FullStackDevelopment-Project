9509411675669c2a7df7b22999b7ef1ae110f164
Ubuntu <ubuntu@ip-172-31-41-89.ec2.internal>
2019-09-05 09:03:31 +0000

    Product Page is now displaying properly using Jinja2

--- crud/urls.py
+++ crud/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from crud.views import show_todos, create_item, edit_item, item_list, HomeView, ItemDetailView
+# from crud.views import show_todos, create_item, edit_item, item_list, HomeView, ItemDetailView
+from crud.views import item_list, product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crud/', show_todos, name='show_todos'),
-    path('crud/create', create_item),
-    path('crud/edit/<id>', edit_item),
-    path('home/', HomeView.as_view(), name='item_list'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product')
+    # path('crud/', show_todos, name='show_todos'),
+    # path('crud/create', create_item),
+    # path('crud/edit/<id>', edit_item),
+    path('home/', item_list, name='item_list'),
+    path('product/<id>', product, name='product')
 ]
--- crud/views.py
+++ crud/views.py
@@ -2,54 +2,57 @@ from django.shortcuts import render
 from .forms import ItemForm
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
-from django.views.generic import ListView, DetailView
+# from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def show_todos(request):
-    results = Item.objects.all()
-    context = {
-        "items" : results
-    }
-    return render(request, 'todo.html', context)
+# def show_todos(request):
+#     results = Item.objects.all()
+#     context = {
+#         "items" : results
+#     }
+#     return render(request, 'todo.html', context)
     
-def create_item(request):
-  if request.method == "POST":
-        form = ItemForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect(show_todos)
-  else:
-     form = ItemForm()
-     return render(request, "create_items.html", {'form': form})
+# def create_item(request):
+#   if request.method == "POST":
+#         form = ItemForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(show_todos)
+#   else:
+#      form = ItemForm()
+#      return render(request, "create_items.html", {'form': form})
 
-def edit_item(request, id):
-    item = get_object_or_404(Item, pk=id)
+# def edit_item(request, id):
+#     item = get_object_or_404(Item, pk=id)
 
-    if request.method == "POST":
-        form = ItemForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save()
-            return redirect(show_todos)
-    else:
-        form = ItemForm(instance=item)
-    return render(request, "create_items.html", {'form': form})
+#     if request.method == "POST":
+#         form = ItemForm(request.POST, instance=item)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(show_todos)
+#     else:
+#         form = ItemForm(instance=item)
+#     return render(request, "create_items.html", {'form': form})
     
 def item_list(request):
+    results = Item.objects.all()
     context = {
-        'items' : Item.objects.all()
+        'items' : results
     }
     return render(request, "home-page.html", context)
     
-class HomeView(ListView):
-    model = Item
-    template_name = "home-page.html"
+# class HomeView(ListView):
+#     model = Item
+#     template_name = "home-page.html"
 
-def product(request):
+def product(request, id):
+    item = get_object_or_404(Item, pk=id)
+    results = Item.objects.all()
     context = {
-        'items' : Item.objects.all()
+        'items' : results
     }
     return render(request, "product-page.html", context)
     
-class ItemDetailView(DetailView):
-    model = Item
-    template_name = "product-page.html"
+# class ItemDetailView(DetailView):
+#     model = Item
+#     template_name = "product-page.html"
