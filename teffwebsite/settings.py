4ef5a3f8d0f2f4ff71bbf5322765bfc48174744c
Ubuntu <ubuntu@ip-172-31-41-89.ec2.internal>
2019-09-06 08:41:04 +0000

    Staging application for deployment

--- teffwebsite/settings.py
+++ teffwebsite/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'teffwebsite.urls'
@@ -122,11 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 AUTH_USER_MODEL = 'accounts.MyUser' 
 
@@ -137,6 +142,3 @@ AUTHENTICATION_BACKENDS = (
 )
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
